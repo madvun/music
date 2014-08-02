b45b6b33ba8d7c3bae373469ba0b27dcd8e8b812
Demko <artdemko@gmail.com>
2014-08-02 16:40:48 +0300

    add index

--- db/migrate/20140802132714_create_users.rb
+++ db/migrate/20140802132714_create_users.rb
@@ -7,8 +7,10 @@ class CreateUsers < ActiveRecord::Migration
       t.string :password_d
       t.string :remember_token
       t.boolean :admin, default: false
-      
+
       t.timestamps
     end
+    add_index :users, :email, unique: true
+    add_index  :users, :remember_token
   end
 end
